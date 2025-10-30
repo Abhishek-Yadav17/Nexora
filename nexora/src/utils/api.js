5f29b931df8b40f9483dcc1d092f5709ed210ae5
Abhishek-Yadav17 <17abhishekadav@gmail.com>
2025-10-30 13:59:00 +0530

    deployed backend

--- nexora/src/utils/api.js
+++ nexora/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://nexora-zaer.onrender.com/api';
 
 export const getProducts = async () => {  
   const res = await axios.get(`${BASE_URL}/products`);
