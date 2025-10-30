976c44d6596cd4b82ff26ad7806092c0902e44d5
Abhishek-Yadav17 <17abhishekadav@gmail.com>
2025-10-30 13:49:23 +0530

    using env variables

--- nexora-backend/server.js
+++ nexora-backend/server.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://17abhishekyadav:4jtrD6usQ1snGXlu@cluster0.5empw.mongodb.net/nexora?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
 
 app.use('/api/products', require('./routes/products'));
 app.use('/api/cart', require('./routes/cart'));
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
